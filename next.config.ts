ba1db2b3e76732266f234bce8136c34481d4f47f
Rajpoot <al.burhan.abn@gmail.com>
2026-04-17 00:14:24 -0700

    hello

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    output: 'export',
+  // Optional: Recommended for static hosts to prevent image optimization errors
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
